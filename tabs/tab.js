fa9cd8802f8eb47a31c39d52cf62911a4a2c08df
kadincool <kadinalone@gmail.com>
2024-02-08 22:20:41 -0700

    Oh no... :(

--- tabs/tab.js
+++ tabs/tab.js
@@ -12,7 +12,7 @@ function changeTab(tab) {
   let tabContents = document.getElementsByClassName("content");
   for (let i=0; i<tabContents.length; i++) {
     if (tabContents[i].id==tab) {
-      tabContents[i].style.display = "block";
+      tabContents[i].style.display = "";
     } else {
       tabContents[i].style.display = "none";
     }
