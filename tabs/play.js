75cc73370d8bfde68cef49e539e636b73cad6ea0
kadin <kadinalone@gmail.com>
2024-02-28 23:59:06 -0700

    the browser seems to get annoyed with my frame methods

--- tabs/play.js
+++ tabs/play.js
@@ -2,8 +2,13 @@ function runProgram() {
   changeTab("Play");
   const canvas = document.getElementById("runCanvas");
   const ctx = canvas.getContext("2d");
-  try {eval(document.getElementById("srcCode").innerText); programFrame();}
+  try {eval(document.getElementById("srcCode").innerText);}
   catch (error) {alert(error); console.log(error)}
+}
+
+function runPerFrame(func) {
+  func();
+  requestAnimationFrame(runPerFrame(func));
 }
 
 function frame() {}
